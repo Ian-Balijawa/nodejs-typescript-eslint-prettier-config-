595172b423b041b87c10a19dc85f6b510fbbea91
ian <ianbalijawa16@gmail.com>
2022-04-30 18:07:50 +0300

    replace String with string in promise return

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-async function main() {
+async function main(): Promise<string> {
   console.log('Main')
+  return new Promise((r, j) => {
+    setTimeout(() => {
+      r('Hello World!')
+    }, 1000) // 1s
+  })
 }
 
-void main()
+async function runMain() {
+  const Hello = await main()
+  return Hello
+}
+
+runMain()
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err))
